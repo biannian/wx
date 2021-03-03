81c97eac2b2564ad7c1e7ad350ec3d1b311c4001
Panguangke <1124784701@qq.com>
2021-03-03 17:10:29 +0800

    首页的实现

---
+++ api/api.js
@@ -0,0 +1,6 @@
+const API = {
+    getOpenid: (data) => request(GET, `jsapi/mini?jsCode=${data}`),
+};
+module.exports = {
+    API: API
+}
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // app.js
+
 App({
   onLaunch() {
     // 展示本地存储能力
@@ -35,5 +36,5 @@ App({
   },
   globalData: {
     userInfo: null
-  }
+  },
 })
