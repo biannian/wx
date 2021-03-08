7cb5d39f9acdff9f8100ce86ac6457c474a63c7d
Panguangke <1124784701@qq.com>
2021-03-08 17:44:03 +0800

    03/08

--- pages/commodity/commodity.js
+++ pages/commodity/commodity.js
@@ -1,7 +1,13 @@
-Page({ 
-data: { 
-  shops:"",
-  inputShowed: false,
-  inputVal: ""
-},
+
+Page({
+  data: {
+    tabs: ["点餐", "评价", "商家"],
+    activeIndex: 0,
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  }
 })
