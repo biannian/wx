66657dd4f11b74902110b6ca78c021845cbdc0c8
biannian <1124784701@qq.com>
2021-03-29 08:03:51 +0800

    111

---
+++ rider/index/index.js
@@ -0,0 +1,12 @@
+ 
+
+Page({
+ 
+  data: {
+    shops:{},
+  }, 
+  onLoad: function (options) { 
+    
+  },
+  
+})
--- utils/request.js
+++ utils/request.js
@@ -2,9 +2,9 @@ const GET = 'GET';
 const POST = 'POST';
 
 // const baseURL = 'http://236y6m4513.zicp.vip';
-// const baseURL = 'http://172.20.10.4:8087';
+const baseURL = 'http://172.20.10.4:8087';
 // const baseURL = 'http://121.5.222.148:8087';
-const baseURL = 'http://localhost:8087';
+// const baseURL = 'http://localhost:8087';
 
 function request(method, url, data) {
     return new Promise(function (resolve, reject) {
