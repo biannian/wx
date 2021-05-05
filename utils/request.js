7d24911da3eb5e00f4c845452da34edbc2bc0a1c
biannian <1124784701@qq.com>
2021-05-05 09:49:37 +0800

    5/5

--- utils/request.js
+++ utils/request.js
@@ -2,9 +2,10 @@ const GET = 'GET';
 const POST = 'POST';
 
 // const baseURL = 'http://236y6m4513.zicp.vip';
+
 // const baseURL = 'http://172.20.10.4:8087';
 // const baseURL = 'http://121.5.222.148:8087';
-const baseURL = 'http://localhost:8087';
+const baseURL = 'http://192.168.1.102:8087';
 // const baseURL = 'http://chat.biannian.top:8087';
 
 function request(method, url, data) {
